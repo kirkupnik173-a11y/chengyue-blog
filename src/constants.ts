999c591823f3ccc22c8a8a2eb65861574246ada5
kirkupnik173-a11y <kirkupnik173@gmail.com>
2026-03-03 18:10:00 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -19,25 +19,25 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/kirkupnik173-a11y/chengyue-blog.git",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "kirkupnik173@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
@@ -46,31 +46,31 @@ export const SOCIALS: Social[] = [
 export const SHARE_LINKS: Social[] = [
   {
     name: "WhatsApp",
-    href: "https://wa.me/?text=",
+    href: "",
     linkTitle: `Share this post via WhatsApp`,
     icon: IconWhatsapp,
   },
   {
     name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
+    href: "",
     linkTitle: `Share this post on Facebook`,
     icon: IconFacebook,
   },
   {
     name: "X",
-    href: "https://x.com/intent/post?url=",
+    href: "",
     linkTitle: `Share this post on X`,
     icon: IconBrandX,
   },
   {
     name: "Telegram",
-    href: "https://t.me/share/url?url=",
+    href: "",
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
   },
   {
     name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
+    href: "",
     linkTitle: `Share this post on Pinterest`,
     icon: IconPinterest,
   },
